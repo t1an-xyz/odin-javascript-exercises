8500d79fc0191a5a0c4177beafa9fea7a2e1b91a
Theo <62950302+t1an-xyz@users.noreply.github.com>
2023-12-17 13:55:23 -0500

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+        let currYear = (new Date()).getFullYear();
+        let thisAge = currYear - curr.yearOfBirth;
+        if ("yearOfDeath" in curr) {
+            thisAge = curr.yearOfDeath - curr.yearOfBirth;
+        }
+        let oldAge = currYear - oldest.yearOfBirth;
+        if ("yearOfDeath" in oldest) {
+            oldAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+        return thisAge > oldAge ? curr : oldest;
+    });
 };
 
 // Do not edit below this line
